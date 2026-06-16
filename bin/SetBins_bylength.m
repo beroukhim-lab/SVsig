61b6c0e272b4d2a4423db5d3e936a0f8b7e14265
ht782 <114322187+harunatomono@users.noreply.github.com>
2026-06-16 15:19:04 -0400

    merging parfor setbins change

--- bin/SetBins_bylength.m
+++ bin/SetBins_bylength.m
@@ -22,12 +22,11 @@ for c1 = CHR
 
     chri = find(chr==c1);
     sorted_pos = sort(pos(chri));
+    num_chr = floor(chsize(c1)/avg_bin_length);
+    avg_length_chr = floor(avg_bin_length + ((chsize(c1)-num_chr*avg_bin_length)/num_chr));
 
     if ~use_shifted_bins
         % Legacy path kept to ensure n_binshifts=0 is identical to existing output.
-        num_chr=floor(chsize(c1)/avg_bin_length);
-        avg_length_chr=floor(avg_bin_length + ((chsize(c1)-num_chr*avg_bin_length)/num_chr));
-
         start1=1;
         for c2 = 1:num_chr
             bins(bc,1) = c1;
@@ -52,14 +51,14 @@ for c1 = CHR
 
         start1 = bin_shift;
         while start1 <= chr_size
-            end1 = min(start1 + avg_bin_length - 1, chr_size);
+            end1 = min(start1 + avg_length_chr - 1, chr_size);
             bins(bc,1) = c1;
             bins(bc,2) = start1;
             bins(bc,3) = end1;
             breaks_per_bin = find(sorted_pos(:,1) <= end1 & sorted_pos(:,1) >= start1);
             bins(bc,4) = length(breaks_per_bin);
             bc = bc + 1;
-            start1 = start1 + avg_bin_length;
+            start1 = start1 + avg_length_chr;
         end
     end
 
